e0161054e30cd7833a7931788c0bc180933dc78b
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-12-30 12:37:50 +0530

    create form with validations

--- InventoryManagementApp/src/app/app.module.ts
+++ InventoryManagementApp/src/app/app.module.ts
@@ -13,7 +13,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core'
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +32,8 @@ import {MatNativeDateModule} from '@angular/material/core'
     MatInputModule,
     MatSelectModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- InventoryManagementApp/src/app/dialog/dialog.component.ts
+++ InventoryManagementApp/src/app/dialog/dialog.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.css']
+  styleUrls: ['./dialog.component.css'],
 })
 export class DialogComponent implements OnInit {
- constructor(){}
- ngOnInit(): void {
-   
- }
-
+  freshnessList = ['Brand New', 'Second Hand', 'Refurbished'];
+  productForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      productName: ['', Validators.required],
+      category: ['', Validators.required],
+      freshness: ['', Validators.required],
+      price: ['', Validators.required],
+      comment: ['', Validators.required],
+    });
+  }
 }
