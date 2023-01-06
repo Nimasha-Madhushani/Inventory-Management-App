4a91e5f8ca99b50e7be75897b3ecdc3c9dc3b1ff
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2023-01-06 19:54:43 +0530

    add paginator

--- InventoryManagementApp/src/app/app.component.ts
+++ InventoryManagementApp/src/app/app.component.ts
@@ -1,21 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'InventoryManagementApp';
+  displayedColumns: string[] = ['productName', 'category', 'date', 'price','comment'];
+  dataSource!: MatTableDataSource<any>;
 
-  constructor(private dialog:MatDialog){
-
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  
+  //Modify app.component.ts file. Define applyFilter method to filter dataSource data
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  constructor(private dialog: MatDialog, private api: ApiService) {}
+  ngOnInit(): void {
+    this.getAllProducts();
   }
   openDialog() {
     this.dialog.open(DialogComponent, {
-      width:'30%'
+      width: '30%',
+    });
+  }
+
+  getAllProducts() {
+    this.api.getProduct().subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        alert('Error while fetching records');
+      },
     });
   }
 }
