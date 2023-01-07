67a39faf401d613dc32ffed9f548a2478483e61f
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2023-01-07 13:44:59 +0530

    update product is completed

--- InventoryManagementApp/src/app/dialog/dialog.component.ts
+++ InventoryManagementApp/src/app/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,Inject} from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
 import { ApiService } from '../services/api.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
@@ -13,7 +13,7 @@ export class DialogComponent implements OnInit {
   fresh: any;
   freshnessList = ['Brand New', 'Second Hand', 'Refurbished'];
   productForm!: FormGroup;
-  actionBtn : string="Save"
+  actionBtn: string = 'Save';
   constructor(
     private formBuilder: FormBuilder,
     private api: ApiService,
@@ -29,30 +29,48 @@ export class DialogComponent implements OnInit {
       price: ['', Validators.required],
       comment: ['', Validators.required],
     });
-    if(this.editData){
-      this.actionBtn="Update"
-      this.productForm.controls['productName'].setValue(this.editData.productName)
-      this.productForm.controls['category'].setValue(this.editData.category)
-      this.productForm.controls['freshness'].setValue(this.editData.freshness)
-      this.productForm.controls['date'].setValue(this.editData.date)
-      this.productForm.controls['price'].setValue(this.editData.price)
-      this.productForm.controls['comment'].setValue(this.editData.comment)
+    if (this.editData) {
+      this.actionBtn = 'Update';
+      this.productForm.controls['productName'].setValue(
+        this.editData.productName
+      );
+      this.productForm.controls['category'].setValue(this.editData.category);
+      this.productForm.controls['freshness'].setValue(this.editData.freshness);
+      this.productForm.controls['date'].setValue(this.editData.date);
+      this.productForm.controls['price'].setValue(this.editData.price);
+      this.productForm.controls['comment'].setValue(this.editData.comment);
     }
     //console.log(this.editData)
   }
 
   addProduct() {
-    if (this.productForm.valid) {
-      this.api.postProduct(this.productForm.value).subscribe({
-        next: (res) => {
-          alert('product added succesfully');
-          this.productForm.reset();
-          this.dialogRef.close();
-        },
-        error: () => {
-          alert('error while adding the product');
-        },
-      });
+    if (!this.editData) {
+      if (this.productForm.valid) {
+        this.api.postProduct(this.productForm.value).subscribe({
+          next: (res) => {
+            alert('product added succesfully');
+            this.productForm.reset();
+            this.dialogRef.close();
+          },
+          error: () => {
+            alert('error while adding the product');
+          },
+        });
+      }
+    } else {
+      this.updateProduct();
     }
+  }
+  updateProduct() {
+    this.api.putProduct(this.productForm.value, this.editData.id).subscribe({
+      next: (res) => {
+        alert('Product updated successfully');
+        this.productForm.reset();
+        this.dialogRef.close('update');
+      },
+      error: () => {
+        alert('Error while updating the record');
+      },
+    });
   }
 }
